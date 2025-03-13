f03e9bfad9e022271fe55a159d43360fd51d4c9a
rachit1412 <114014266+rachit1412@users.noreply.github.com>
2025-03-14 01:25:53 +0530

    Bind application to 0.0.0.0 and use Render's port environment variable

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
+import os
 
 app = Flask(__name__)
 
@@ -72,4 +73,5 @@ def index():
     return render_template('index.html', result='')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
